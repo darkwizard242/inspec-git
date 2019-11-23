c2a2a036fc473edab8d850d177b2dc1f5a927a7b
darkwizard242 <am900820@gmail.com>
2019-11-23 00:28:38 -0500

    set up inspec profile for git

---
+++ README.md
@@ -0,0 +1,9 @@
+# InSpec Compliance Profile: git
+
+## Description:
+
+The function of this profile is to verify and validate whether git is installed on Ubuntu 18.04 system and can be utilized.
+
+## Requirements:
+
+- [InSpec](https://github.com/chef/inspec)
---
+++ controls/01_git.rb
@@ -0,0 +1,27 @@
+# copyright: 2019, Ali Muhammad
+
+title "Compliance git"
+
+control "git-01" do
+  impact 0.7
+  title "Validate git is installed and ready to be used."
+  desc "This compliance test will validate whether git is installed on the system. It will also attempt verification for the command execution."
+
+  describe package("git") do
+    it { should be_installed }
+  end
+  describe command('which git') do
+    its('exit_status') { should eq 0 }
+    its('stdout') { should eq "/usr/bin/git\n" }
+  end
+end
+
+control "git-02" do
+  impact 0.7
+  title "Verify git is operational."
+  desc "This control will also whether git is operational or not."
+
+  describe command('git --version') do
+    its('exit_status') { should eq 0 }
+  end
+end
